736fb275bd85c9311f4772df33aa6c44bb106bda
Vitor Teixeira <vitorfox@gmail.com>
2013-12-21 17:28:29 -0200

    .gitignore is now working

--- gen/com/vitorteixeira/android/tracker/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.vitorteixeira.android.tracker;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
